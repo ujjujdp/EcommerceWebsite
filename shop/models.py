de0fed7b67d53a960ac65462b6f01cfda424d652
Ujjawal Dewangan <ujjawal75871@gmail.com>
2021-05-28 19:29:12 +0530

    End of the Project

--- shop/models.py
+++ shop/models.py
@@ -11,8 +11,16 @@ class Product(models.Model):
     desc = models.CharField(max_length=300)
     pub_data = models.DateField()   
     image = models.ImageField(upload_to="shop/images",default="")
-     
+
     def __str__(self):
         return self.product_name
 
+class Contact(models.Model):
+    query_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70,default="")
+    phno = models.CharField(max_length=70,default="")
+    descr = models.CharField(max_length=50,default="")
+    def __str__(self):
+        return self.user_name
     
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 # Create your views here.
 def index(request):
@@ -28,6 +28,19 @@ def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
+    if request.method=="POST":
+        print(request)
+
+    name = request.POST.get('name','')
+    email = request.POST.get('email','')
+    phno = request.POST.get('phno','')
+    descr = request.POST.get('descr','')
+    print(name)
+    print(email)
+    print(phno)
+    print(descr)
+    contact = Contact(user_name = name, email = email, phno=phno, descr = descr)
+    contact.save()
     return render(request,'shop/contact.html')
 
 def track(request):
